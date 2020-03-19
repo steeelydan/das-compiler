0bb80b79cbedaf9f15e0ce943bda0604ca06f508
Steeely Dan <steeelydan@posteo.de>
2020-03-19 15:26:03 +0100

    WIP Tokenizer

--- compile.js
+++ compile.js
@@ -7,11 +7,27 @@ const fs = require('fs');
 
 class Tokenizer {
     constructor() {
+        // Order is important!
+        this.tokenTypes = new Map();
+        this.tokenTypes.set('def', `\\bdef\\b`);
+        this.tokenTypes.set('end', '\\bend\\b');
+        this.tokenTypes.set('identifier', '\\b[a-zA.Z]+\\b');
+        this.tokenTypes.set('integer', '\\b[0-9]+\\b');
+        this.tokenTypes.set('oparen', '\\(');
+        this.tokenTypes.set('cparen', '\\)');
+
         this.source = fs.readFileSync('test.src', { encoding: 'utf8' }).trim();
     }
 
     tokenize() {
         console.log(this.source);
+        while (this.source !== '') {
+            this.tokenTypes.forEach((value, key) => {
+                const regex = new RegExp('\\A(' + value + ')');
+                console.log(regex, value);
+                console.log(this.source.match(regex));
+            });
+        }
     }
 }
 
